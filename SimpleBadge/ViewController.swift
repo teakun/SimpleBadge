385c883172d01944627765289e669e3ccddfaf26
TAKEDA Yuki <teakun@kittengine.com>
2019-09-24 23:09:06 +0900

    Initial Commit

---
+++ SimpleBadge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleBadge
+//
+//  Created by teakun on 2019/09/24.
+//  Copyright © 2019 kittengine.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
